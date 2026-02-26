cec7bb93eee8a6e54ad9b26f18d739639cdd3bdc
dcpurnell <dcpurnell@gmail.com>
2026-02-25 19:28:08 -0500

    Update .gitignore and remove build artifacts

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import sql from "mssql";
-export declare function createSqlConfig(): Promise<{
-    config: sql.config;
-    token: string;
-    expiresOn: Date;
-}>;
-//# sourceMappingURL=index.d.ts.map
